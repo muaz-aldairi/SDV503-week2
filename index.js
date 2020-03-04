c6bc0ebe80b9fbe6998ee212989070b965e359f5
Muaz Aldairi <muaz-aldairi@live.nmit.ac.nz>
2020-03-04 15:59:40 +1300

    moving the code

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello class")
+var firstName;
+firstName= "Muaz";
+console.log(firstName);
+
+var lastName= "Aldairi";
+console.log(lastName);
+
+console.log(firstName + " " +lastName);
+
+var firstNumber= 8;
+var  secondNumber= 6;
+console.log("sum=" + " " + (firstNumber + lastNumber));
+console.log(firstNumber-secondNumber);
+console.log(firstNumber/secondNumber);
+
