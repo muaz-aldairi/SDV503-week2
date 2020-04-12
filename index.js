62eb5352b866a49c7026e90ce45fe5aeee2b1847
Muaz <muaz-aldairi@live.nmit.ac.nz>
2020-04-13 09:02:31 +1200

    Arithmetic Operator

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ console.log(firstName + " " +lastName);
 
 var firstNumber= 8;
 var  secondNumber= 6;
-console.log("sum=" + " " + (firstNumber + lastNumber));
-console.log(firstNumber-secondNumber);
-console.log(firstNumber/secondNumber);
+console.log("sum=" + " " + (firstNumber + secondNumber));
+console.log("sub=" + " " + (firstNumber - secondNumber));
+console.log("multi=" + " " + (firstNumber * secondNumber));
+console.log("div=" + " " + (firstNumber/ secondNumber));
+
 
